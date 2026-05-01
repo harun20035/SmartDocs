110a03958cf569dd066a266dae6ce6368e7036a4
Harun Hasagić <harunhasagic2@gmail.com>
2026-05-01 15:52:38 +0200

    allowed frontend deploy route by cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://smart-docs-orcin.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://smart-docs-ashen.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
