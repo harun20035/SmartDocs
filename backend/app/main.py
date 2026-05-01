c6644578f41bd1c0751fe729f7784b8217477b99
Harun Hasagić <harunhasagic2@gmail.com>
2026-05-01 15:43:48 +0200

    added vercel deploy route to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://smart-docs-orcin.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
