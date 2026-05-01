862f475a48ed699ad6b9eff61e15b0493b849f53
Harun Hasagić <harunhasagic2@gmail.com>
2026-05-01 16:01:13 +0200

    added new frontend access route to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://smart-docs-ashen.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://smart-docs-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
